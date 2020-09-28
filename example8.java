8e6c7caa465c22170314ca32fcac8972bad43bd1
GauravShinde-22 <72022625+GauravShinde-22@users.noreply.github.com>
2020-09-28 18:07:51 +0530

    Add files via upload

---
+++ example8.java
@@ -0,0 +1,26 @@
+class example8
+{
+ public static void main(String args[])
+ {
+  String s1="   J     a    V      V    a";
+  String s2="   J    a a    V    V    a a";
+  String s3="J  J   aaaaa    V  V    aaaaa";
+  String s4=" JJ  a      a    VV    a     a";
+  for(int i=;i==1;i++)
+  {
+   System.out.println(s1);
+  }
+    for(int j=1;j==1;j++)
+  {
+   System.out.println(s2);
+  }
+    for(int k=1;k==1;k++)
+  {
+   System.out.println(s3);
+  }
+  for(int l=1;l==1;l++)
+  {
+   System.out.println(s4);
+  }
+ }
+}
